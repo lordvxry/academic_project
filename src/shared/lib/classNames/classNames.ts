7253fb5ab238afeed0b89f87d57244d3c9e63a54
vladimirsmolin <lordvxry@gmail.com>
2023-05-30 19:47:34 +0400

    initialized project

---
+++ src/shared/lib/classNames/classNames.ts
@@ -0,0 +1,11 @@
+type Modes = Record<string, boolean | string>;
+
+export const classNames = (className: string, modes: Modes = {}, additional: string[] = []): string => {
+    return [
+        className,
+        ...additional.filter(Boolean),
+        Object.keys(modes)
+            .filter(key => modes[key])
+    ]
+        .join(' ')
+};
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,25 @@
+import React, {FC} from 'react';
+import cls from './Navbar.module.scss';
+import {classNames} from 'shared/lib/classNames/classNames';
+import {AppLink, AppLinkTheme} from 'shared/ui/AppLink/AppLink';
+
+interface NavbarProps {
+    className?: string;
+}
+
+export const Navbar: FC<NavbarProps> = (props) => {
+    const {className} = props;
+
+    return (
+        <div className={classNames(cls.navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink theme={AppLinkTheme.INVERTED} to={'/'} className={cls.mainLink}>
+                    Main
+                </AppLink>
+                <AppLink theme={AppLinkTheme.INVERTED} to={'/about'}>
+                    About
+                </AppLink>
+            </div>
+        </div>
+    );
+};
